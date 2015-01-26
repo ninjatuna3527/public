4dc6829d8007f0dbd6b837a5aee95d697ea9dc3c
Richard Leeson <richardjleeson@gmail.com>
2015-01-26 01:30:29 +0000

    Update install-agda-emacs.sh

--- install-agda-emacs.sh
+++ install-agda-emacs.sh
@@ -5,8 +5,8 @@ sudo apt-get install emacs-snapshot emacs-snapshot-el Agda
 mkdir -p ~/emacs.d/site-lisp/
 wget -P ~/emacs.d/site-lisp/ http://code.haskell.org/Agda/src/data/emacs-mode/agda2-mode.el
 touch ~/.emacs
-echo "(add-to-list "\'"load-path \"~/.emacs.d/site-lisp/agda2-mode\")" >> ~/.emacs
-echo "(require "\'"agda2-mode)" >> ~/.emacs
+echo "(add-to-list 'load-path \"~/.emacs.d/site-lisp/agda2-mode\")" >> ~/.emacs
+echo "(require 'agda2-mode)" >> ~/.emacs
 echo "" >> ~/.emacs
 echo "install completed successfully!!"
 emacs &
