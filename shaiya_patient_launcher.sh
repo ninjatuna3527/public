67c4384e7db1c0b6e70d3e761b58fe8ed0cd5c51
Richard Leeson <richardjleeson@gmail.com>
2015-04-30 00:35:49 +0100

    Update shaiya_patient_launcher.sh

--- shaiya_patient_launcher.sh
+++ shaiya_patient_launcher.sh
@@ -1,10 +1,11 @@
-#!/bin/python
+#!/usr/bin/env python2
 __author__="Richard Leeson"
 import socket
 from datetime import datetime
 import time
 import os
 import platform
+import sys
 
 #check privs (need admin to instant start shaiya)
 try:
@@ -22,25 +23,26 @@ else:
 shaiya_ip="108.163.135.198"
 shaiya_port=30800
 
-#socket setup
-TIMEOUT=0.4
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(TIMEOUT)
-
 #status
 tries=0
 start_time = datetime.now()
 
 while True:
   try:
+	#socket setup
+    TIMEOUT=0.4
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(TIMEOUT)
     s.connect((shaiya_ip,shaiya_port,))
     if s.recv(1024):
       for i in range(0,10):
         print "\a"
         time.sleep(0.5)
       os.system(shaiya_cmd)
+      sys.exit(0)
   except (socket.timeout, socket.error) as e:
     tries+=1
     os.system("cls")
-    print "started: {0}\ntries:{1}\ntime elapsed:{2}".format(start_time, tries, (datetime.now()-start_time).strftime("%Hh - %Mm - %Ss"))
+    print "started: {0}\ntries:{1}\ntime elapsed:{2}".format(start_time, tries, str(datetime.now()-start_time))
+    time.sleep(5)
   
